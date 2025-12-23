2b0510231f0482256f166407632bd9320efc7491
DanielBekelee <daniel.bekele.garedew@gmail.com>
2025-12-23 09:19:33 +0300

    Brute-Force Protection

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -5,13 +5,16 @@ import { registerUser, loginUser } from "../controllers/authController.js";
 import { updatePassword } from "../controllers/updatePasswordController.js";
 import { forgotPassword } from "../controllers/forgotPassworController.js";
 import { resetPassword } from "../controllers/resetPasswordController.js";
+import { forgotPasswordLimiter } from "../middleware/rateLimitMiddleware.js";
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.put("/update-password", protect, updatePassword);
 //forgot password
-router.post("/forgot-password", forgotPassword);
+router.post("/forgot-password",forgotPasswordLimiter, forgotPassword);
 // Reset password
 router.put("/reset-password/:token", resetPassword);
+
+
 export default router;
